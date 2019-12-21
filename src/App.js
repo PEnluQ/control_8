3a745f185aaaec23cdd75ebea62ea12773d3a445
dell <khasyu@mail.ru>
2019-12-21 18:44:04 +0600

    Finish

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Navigation from "./components/Navigation/Navigation";
+import Quotes from "./containers/Quotes/Quotes";
+import AddNew from "./containers/AddNew/AddNew";
+import Edit from "./containers/Edit/Edit";
+
 
 const App = () => {
     return (
-        <div>
-
-        </div>
+        <BrowserRouter>
+            <Navigation/>
+            <Switch>
+                <Route path="/" exact component={Quotes}/>
+                <Route path="/categories/:name" component={Quotes}/>
+                <Route path="/quotes/new" component={AddNew}/>
+                <Route path="/quotes/:id" component={Edit}/>
+                <Route render={() => <h1>Not found</h1>}/>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
--- src/containers/Quotes/Quotes.js
+++ src/containers/Quotes/Quotes.js
@@ -1,11 +1,68 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import axiosQuotes from "../../axios-quotes";
+import Posts from "../../components/Posts/Posts";
+import {CATEGORIES} from "../../constants";
+import {NavLink} from "react-router-dom";
 
 class Quotes extends Component {
+    state = {
+      quotes: [],
+    };
+
+    requestInfo = async () => {
+        let url = '/quotes.json';
+
+        if(this.props.match.params.name) {
+            url += `?orderBy="category"&equalTo="${this.props.match.params.name}"`
+        }
+
+        const response = await axiosQuotes.get(url);
+
+        if (response.data) {
+            this.setState({quotes: response.data});
+        }
+    };
+
+    async componentDidMount() {
+        this.requestInfo();
+    };
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.match.params.name !== this.props.match.params.name){
+           return this.requestInfo();
+        }
+    }
+
+    removeQuote = async (id) => {
+      const remove = await axiosQuotes.delete('/quotes/' + id + '.json');
+      if(remove.statusText === 'OK'){
+          return this.requestInfo();
+      }
+    };
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <Fragment>
+                <ul>
+                    <li><NavLink to={'/'}>{CATEGORIES[0]}</NavLink></li>
+                    {CATEGORIES.filter(item => (
+                        item !== 'All'
+                    )).map(c => (
+                        <li key={c}>
+                            <NavLink to={'/categories/' + c}>{c}</NavLink>
+                        </li>
+                    ))}
+                </ul>
+                {Object.keys(this.state.quotes).map(id => (
+                    <Posts
+                        key={id}
+                        onCLick={() => this.removeQuote(id)}
+                        name={this.state.quotes[id].author}
+                        text={this.state.quotes[id].quote}
+                        to={"/quotes/" + id}
+                    />
+                ))}
+            </Fragment>
         );
     }
 }
